2eb44503bbc29580825480e1957a1372492d3d95
ankhell <2011Hellangel@gmail.com>
2025-07-05 19:05:14 +0200

    Initial commit

---
+++ src/main/kotlin/github/ankhell/bank_bot/BankBotApplication.kt
@@ -0,0 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankBotApplication>(*args)
+}
