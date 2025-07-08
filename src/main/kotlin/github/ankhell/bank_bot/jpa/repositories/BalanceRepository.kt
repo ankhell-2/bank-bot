e959701659f393859e2ea73fb4de081848d03d90
ankhell <2011Hellangel@gmail.com>
2025-07-08 06:13:04 +0200

    BalanceRepository fix

--- src/main/kotlin/github/ankhell/bank_bot/jpa/repositories/BalanceRepository.kt
+++ src/main/kotlin/github/ankhell/bank_bot/jpa/repositories/BalanceRepository.kt
@@ -6,7 +6,7 @@ import github.ankhell.bank_bot.jpa.entities.Guild
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BalanceRepository : JpaRepository<Balance, Bank>{
-    fun findByBankOrNull(bank: Bank): Balance?
+    fun findByBank(bank: Bank): Balance?
 
     fun findAllByGuild(guild: Guild): Set<Balance>
 }
