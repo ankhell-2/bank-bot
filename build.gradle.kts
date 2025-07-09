22b6808919f4041921678511240eaa20ec3fa69f
ankhell <2011Hellangel@gmail.com>
2025-07-10 01:06:55 +0200

    Replaced auto ddl with liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.liquibase:liquibase-core")
 
 
 	testImplementation("com.h2database:h2:2.3.232")
